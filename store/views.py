72d7c36a90471bbd18333760db5497da59abf3cd
Axel Alba <albaaxel@yahoo.com>
2020-08-10 21:06:24 +0800

    Data structure and page rendering implemented.

--- store/views.py
+++ store/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import * 
 
 def store(request):
-  context = {}
-  return render(request, 'store/store.html', context)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'store/store.html', context)
 
 def cart(request):
-  context = {}
-  return render(request, 'store/cart.html', context)
 
-def checkout(request):
-  context = {}
-  return render(request, 'store/checkout.html', context)
+	if request.user.is_authenticated: #if the user is logged in
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False) #creating an object if it does not exist, else only query
+		items = order.orderitem_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+
+	context = {'items':items, 'order':order}
+	return render(request, 'store/cart.html', context)
+
+def checkout(request): #same with cart
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+
+	context = {'items':items, 'order':order}
+	return render(request, 'store/checkout.html', context)
